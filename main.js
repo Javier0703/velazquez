5772d0cdc41e9a302144b68f389009381d5d5952
Javier <javiercalvoporro@gmail.com>
2022-10-15 21:22:16 +0200

    JS PopUp

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ tabs.forEach((tab, index) => {
         tabs[index].classList.add('is-active')
     });
 });
+
+document.querySelectorAll('.tabs_content img').forEach(image =>{
+    image.onclick = () => {
+        document.querySelector('.popup-imagen').style.display = 'block';
+        document.querySelector('.popup-imagen img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-imagen').onclik = () => {
+    document.querySelector('.popup-imagen').style.display = 'none';
+};
+
+
+function cambiar() {
+    var imagen = document.getElementById("popup");
+    if(imagen.style.display  === "none"){imagen.style.display  = "block";     }
+    else {imagen.style.display  = "none";
+    }};
+
+
